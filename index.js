642c8ada46541c281b95b34d48bf1e68799f18ab
Sushi2021 <72.probal@gmail.com>
2024-03-20 01:30:31 +0530

    commit

---
+++ index.js
@@ -0,0 +1,23 @@
+    //1. Ways to print in JavaScript
+    //console.log("Hello World");
+    //alert("Me");
+    // document.write("Hello World");
+
+    //2. Javascript console API
+    console.log("Hello World");
+    console.warn("This is a warning");
+    console.error("This is an error");
+
+    //3. JavaScript Variables
+    // What are Variables? - Containers to store data values
+    var number1 = 34;
+    var number2 = "34";
+    console.log(number1 + number2);
+
+    //4. Data types in JavaScript
+    // Numbers: Integers, Floats, Hex
+    var num1 = 455;
+    var num2 = 56.76;
+    var num3 = 0xFF;
+    console.log("Number :" + num1 + " " + num2 + " " + num3);
+    
